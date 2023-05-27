c0f6a8084f0eb1da054f9db2bafa41c000c003b0
Jaime Daza <jdaza17@cuc.edu.co>
2023-05-26 21:35:03 -0500

    feat(config): Create entries for key config

--- src/config/app.js
+++ src/config/app.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 const appConfig = {
   port: process.env.PORT || 3000,
+  privateKey: process.env.JWT_PRIVATE_KEY_FILE,
+  publicKey: process.env.JWT_PUBLIC_KEY_FILE
 };
 
 export default appConfig;
