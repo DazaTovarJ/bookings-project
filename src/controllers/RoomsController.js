765066047be7208a9caa23fc59f8ebc0b3ec86b5
Jaime Daza <jdaza17@cuc.edu.co>
2023-05-29 17:20:21 -0500

    feat(rooms): Create new rooms creation functionality

--- src/controllers/RoomsController.js
+++ src/controllers/RoomsController.js
@@ -1,5 +1,8 @@
 import {Router} from "express";
-import {getRoomById, getRooms} from "../services/RoomsService.js";
+
+import { createRoom, getRoomById, getRooms } from "../services/RoomsService.js";
+import { asyncHandler } from "../middleware/async_handler.js";
+import { ClientError } from "../exceptions/ClientError.js";
 
 const router = Router();
 
@@ -8,31 +11,54 @@ router.get("/", async (req, res) => {
     const rooms = await getRooms();
 
     if (rooms.length == 0) {
-      return res.status(404).json({message: "No rooms found"});
+      return res.status(404).json({ message: "No rooms found" });
     }
 
     res.json(rooms);
   } catch (e) {
-    return res.status(500).json({message: e.message});
+    return res.status(500).json({ message: e.message });
   }
 });
 
 router.get("/:id", async (req, res) => {
   if (!req.params.id || Number.isNaN(req.params.id)) {
-    return res.status(400).json({message: "Invalid booking id"});
+    return res.status(400).json({ message: "Invalid booking id" });
   }
 
   try {
     const room = await getRoomById(req.params.id);
 
     if (!room) {
-      return res.status(404).json({message: "Room not found"});
+      return res.status(404).json({ message: "Room not found" });
     }
 
     return res.json(room);
   } catch (e) {
-    return res.status(500).json({message: e.message});
+    return res.status(500).json({ message: e.message });
   }
 });
+
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    let { room_number, room_type, room_value } = req.body;
+
+    if (!(room_number && room_type && room_value)) {
+      throw new ClientError("Missing required data");
+    }
+
+    if (room_value < 0) {
+      throw new ClientError("Invalid price");
+    }
+
+    await createRoom(req.user, {
+      number: room_number,
+      type: room_type,
+      value: room_value,
+    });
+
+    res.status(201).json({ message: "Room created" });
+  })
+);
 
 export default router;
--- src/services/RoomsService.js
+++ src/services/RoomsService.js
@@ -14,3 +14,32 @@ export async function getRoomById(id) {
 
   return rooms[0];
 }
+
+export async function createRoom(user, room) {
+  const connection = await pool.getConnection();
+
+  await connection.beginTransaction();
+  try {
+    let sql = `INSERT INTO rooms (room_number, room_type, room_value, created_by)
+                 VALUES (?, ?, ?, ?)`;
+
+    const [result] = await connection.execute(sql, [
+      room.number,
+      room.type,
+      room.value,
+      user.id,
+    ]);
+
+    connection.unprepare(sql);
+
+    await connection.commit();
+
+    return result.insertId;
+  } catch (e) {
+    await connection.rollback();
+    throw e;
+  } finally {
+    connection.release();
+  }
+}
+
