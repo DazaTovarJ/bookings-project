df58604c9fbcf38a0c82add9fd183d21ea4869c3
Jaime Daza <jdaza17@cuc.edu.co>
2023-06-01 09:35:45 -0500

    refactor(bookings): Wrap async operations into handlers

--- src/controllers/BookingsController.js
+++ src/controllers/BookingsController.js
@@ -4,6 +4,8 @@ import {
   deleteBooking,
   updateBooking,
 } from "../services/BookingsService.js";
+import { ClientError } from "../exceptions/ClientError.js";
+import { asyncHandler } from "../middleware/async_handler.js";
 
 const router = Router();
 
@@ -11,38 +13,35 @@ router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.post("/", async (req, res) => {
-  const {name, phone, booking_date, check_in, check_out, room} = req.body;
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, phone, booking_date, check_in, check_out, room } = req.body;
 
-  if (!name || !phone || !booking_date || !check_in || !check_out || !room) {
-    return res.status(400).json({message: "Missing required fields"});
-  }
+    if (!(name && phone && booking_date && check_in && check_out && room)) {
+      throw new ClientError("Missing required fields");
+    }
 
-  if (new Date(booking_date).toString() === "Invalid Date") {
-    return res.status(400).json({message: "Invalid booking date"});
-  }
+    if (new Date(booking_date).toString() === "Invalid Date") {
+      throw new ClientError("Invalid booking date");
+    }
 
-  if (new Date(check_in).toString() === "Invalid Date") {
-    return res.status(400).json({message: "Invalid check in date"});
-  }
+    if (new Date(check_in).toString() === "Invalid Date") {
+      throw new ClientError("Invalid check in date");
+    }
 
-  if (new Date(check_out).toString() === "Invalid Date") {
-    return res.status(400).json({message: "Invalid check out date"});
-  }
+    if (new Date(check_out).toString() === "Invalid Date") {
+      throw new ClientError("Invalid check out date");
+    }
 
-  if (new Date(booking_date) >= new Date()) {
-    return res
-      .status(400)
-      .json({message: "Booking date must be in the future"});
-  }
+    if (new Date(booking_date) >= new Date()) {
+      throw new ClientError("Booking date must be in the future");
+    }
 
-  if (new Date(check_in) > new Date(check_out)) {
-    return res
-      .status(400)
-      .json({message: "Check in date must be before check out date"});
-  }
+    if (new Date(check_in) > new Date(check_out)) {
+      throw new ClientError("Check in date must be before check out date");
+    }
 
-  try {
     await createBooking({
       client_name: name,
       client_phone: phone,
@@ -52,88 +51,78 @@ router.post("/", async (req, res) => {
       room_id: room,
     });
 
-    return res.status(201).json({message: "Booking created"});
-  } catch (e) {
-    return res.status(500).json({message: e.message});
-  }
-});
+    return res.status(201).json({ code: 201, message: "Booking created" });
+  })
+);
 
-router.patch("/:id", async (req, res) => {
-  if (!req.params.id || Number.isNaN(Number(req.params.id))) {
-    return res.status(400).json({message: "Invalid booking id"});
-  }
-
-  const {name, phone, booking_date, check_in, check_out, room} = req.body;
-
-  if (
-    booking_date != undefined &&
-    new Date(booking_date).toString() === "Invalid Date"
-  ) {
-    return res.status(400).json({message: "Invalid booking date"});
-  }
-
-  if (
-    check_in != undefined &&
-    new Date(check_in).toString() === "Invalid Date"
-  ) {
-    return res.status(400).json({message: "Invalid check in date"});
-  }
-
-  if (
-    check_out != undefined &&
-    new Date(check_out).toString() === "Invalid Date"
-  ) {
-    return res.status(400).json({message: "Invalid check out date"});
-  }
-
-  const bookingToUpdate = {
-    client_name: name,
-    client_phone: phone,
-    booking_date,
-    entry_date: check_in,
-    end_date: check_out,
-    room_id: room,
-  };
-
-  for (let key in bookingToUpdate) {
-    if (bookingToUpdate[key] === undefined) {
-      delete bookingToUpdate[key];
-    }
-  }
-
-  if (Object.keys(bookingToUpdate).length === 0) {
-    return res.status(400).json({message: "No fields to update"});
-  }
-
-  try {
-    let updated = await updateBooking(req.params.id, bookingToUpdate);
-
-    if (!updated) {
-      return res.status(404).json({message: "Booking not found"});
-    }
-    return res.json({message: "Booking updated successfully"});
-  } catch (e) {
-    console.trace(e);
-    return res.status(500).json({message: e.message});
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  if (!req.params.id || Number.isNaN(Number(req.params.id))) {
-    return res.status(400).json({message: "Invalid booking id"});
-  }
-
-  try {
-    let deleted = await deleteBooking(req.params.id);
-
-    if (!deleted) {
-      return res.status(404).json({message: "Booking not found"});
+router.patch(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    if (!req.params.id || Number.isNaN(Number(req.params.id))) {
+      throw new ClientError("Invalid booking");
     }
 
-    return res.json({message: "Booking deleted"});
-  } catch (e) {
-    return res.status(500).json({message: e.message});
-  }
-});
+    const { name, phone, booking_date, check_in, check_out, room } = req.body;
+
+    if (
+      booking_date != undefined &&
+      new Date(booking_date).toString() === "Invalid Date"
+    ) {
+      throw new ClientError("Invalid booking date");
+    }
+
+    if (
+      check_in != undefined &&
+      new Date(check_in).toString() === "Invalid Date"
+    ) {
+      throw new ClientError("Invalid check in date");
+    }
+
+    if (
+      check_out != undefined &&
+      new Date(check_out).toString() === "Invalid Date"
+    ) {
+      throw new ClientError("Invalid check out date");
+    }
+
+    const bookingToUpdate = {
+      client_name: name,
+      client_phone: phone,
+      booking_date,
+      entry_date: check_in,
+      end_date: check_out,
+      room_id: room,
+    };
+
+    for (let key in bookingToUpdate) {
+      if (bookingToUpdate[key] === undefined) {
+        delete bookingToUpdate[key];
+      }
+    }
+
+    if (Object.keys(bookingToUpdate).length === 0) {
+      throw new ClientError("No fields to update");
+    }
+
+    await updateBooking(req.params.id, bookingToUpdate);
+
+    return res
+      .status(200)
+      .json({ code: 200, message: "Booking updated successfully" });
+  })
+);
+
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    if (!req.params.id || Number.isNaN(Number(req.params.id))) {
+      throw new ClientError("Invalid booking");
+    }
+
+    await deleteBooking(req.params.id);
+
+    return res.status(200).json({ code: 200, message: "Booking deleted" });
+  })
+);
 
 export default router;
