0d3770014e17b6800832365ed27e8c1d2291aacc
Jaime Daza <jaimedavid250701@outlook.com>
2023-11-27 18:04:10 -0500

    refactor(api): Add jwt authentication failure middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,28 +7,42 @@ import roomsRouter from "../controllers/RoomsController.js";
 import userRouter from "../controllers/UserController.js";
 import authRouter from "../controllers/AuthController.js";
 import {checkPermission} from "../middleware/check_permissions.js";
+import {UnauthorizedError} from "../exceptions/UnauthorizedError.js";
+import {NotFoundError} from "../exceptions/NotFoundError.js";
 
 const apiRouter = Router();
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+function jwtAuth(req, res, next) {
+  passport.authenticate("jwt", {session: false}, (err, user) => {
+    if (err) {
+      if (err instanceof NotFoundError)
+        throw new UnauthorizedError("Unauthorized");
+      return next(err);
+    }
+
+    if (!user) throw new UnauthorizedError("Unauthorized");
+
+    req.user = user;
+
+    return next();
+  })(req, res, next);
+}
 
 apiRouter.use(ensureJSONResponse);
 apiRouter.use(
   "/bookings",
-  passport.authenticate("jwt", {session: false}),
+  jwtAuth,
   checkPermission("bookings"),
   bookingsRouter,
 );
-apiRouter.use(
-  "/rooms",
-  passport.authenticate("jwt", {session: false}),
-  checkPermission("rooms"),
-  roomsRouter,
-);
-apiRouter.use(
-  "/users",
-  passport.authenticate("jwt", {session: false}),
-  checkPermission("users"),
-  userRouter,
-);
+apiRouter.use("/rooms", jwtAuth, checkPermission("rooms"), roomsRouter);
+apiRouter.use("/users", jwtAuth, checkPermission("users"), userRouter);
 apiRouter.use("/auth", authRouter);
 
 apiRouter.all("*", (req, res) => {
